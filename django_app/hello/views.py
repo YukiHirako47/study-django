b9de101ac25a6eeb075fcb9310b24ddccba73802
YukiHirako47 <yuuki.i.cosmos@gmail.com>
2021-04-14 09:18:13 +0900

    リスト3-27：CRUDのcreate

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Friend
 from .forms import HelloForm
 
 def index(request):
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
-        'message': 'all friends.',
-        'form':HelloForm(),
-        'data': [],
+        'data': data,
     }
-    if (request.method == 'POST'):
-        num=request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
+
+def create(request):
+    params = {
+        'title': 'Hello',
+        'form': HelloForm(),
+    }
+    if(request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = int(request.POST['age'])
+        birth = request.POST['birthday']
+        friend = Friend(name=name,mail=mail,gender=gender,\
+            age=age,birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request,'hello/create.html',params)
